ee50e08410449af8894b591de56d88402e8fb8d2
Kerin Cosford <cosford@gmail.com>
2011-10-09 23:05:17 +0100

    Added setup.py and license

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-googleplaces',
+    version='0.1',
+    author='Kerin Cosford',
+    author_email='cosford@gmail.com',
+    url='https://github.com/kerin/python-googleplaces/',
+    packages=['googleplaces',],
+    license='BSD',
+    long_description=open('README').read(),
+)
